d0d281e4d645cc43ca542c71b0937c6e9254345d
Marcos Remar <marcosremar@gmail.com>
2017-07-30 23:54:11 -0300

    first commit

---
+++ vue2/src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import {Time} from './time';
+
+require('style!css!bootstrap/dist/css/bootstrap.min.css');
+require('bootstrap');
+new Vue({
+  el: '#app',
+  data: {
+  	times: [
+  		new Time("America Mineiro", require('./assets/america_mg_60x60.png')),
+  		new Time("Atletico Paranaense", require('./assets/atletico-pr_60x60.png')),
+  		new Time("Atletico Mineiro", require('./assets/atletico_mg_60x60.png'))
+  		],
+	alfabeto : {
+		a: 'A',
+		b: 'B',
+		c: 'C',
+		d: 'D',
+		e: 'E'
+	}
+
+  }
+})
