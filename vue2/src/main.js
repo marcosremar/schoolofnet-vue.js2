570244b08aa0ec020c3f14ead8c44fb15a4c76d5
Marcos Remar <marcosremar@gmail.com>
2017-08-03 23:27:52 -0300

    Placa do jogo

--- vue2/src/main.js
+++ vue2/src/main.js
@@ -12,13 +12,31 @@ new Vue({
   		new Time("Atletico Paranaense", require('./assets/atletico-pr_60x60.png')),
   		new Time("Atletico Mineiro", require('./assets/atletico_mg_60x60.png'))
   		],
-	alfabeto : {
-		a: 'A',
-		b: 'B',
-		c: 'C',
-		d: 'D',
-		e: 'E'
-	}
+      novoJogo: {
+        casa: {
+          time: null,
+          gols: 0
+        },
+        fora: {
+          time: null,
+          gols:0
+        }
+      },
+    	alfabeto : {
+    		a: 'A',
+    		b: 'B',
+    		c: 'C',
+    		d: 'D',
+    		e: 'E'
+    	}
+  },
+  created(){
+    let indexCasa = Math.floor(Math.random() * 3); 
+    let indexFora = Math.floor(Math.random() * 3);
 
+    this.novoJogo.casa.time = this.times[indexCasa];
+    this.novoJogo.casa.gols = 0;
+    this.novoJogo.fora.time = this.times[indexFora];
+    this.novoJogo.fora.gols = 0;
   }
 })
